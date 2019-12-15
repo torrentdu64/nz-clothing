ac636d348c503036e743bd3c4ad6e72f3d1fca9b
torrent benoit <torrentdu64@gmail.com>
2019-12-16 12:19:29 +1300

    custom button sa mere trop chian

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 
@@ -17,6 +18,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/Shop' component={ShopPage} />
+        <Route path='/Signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
