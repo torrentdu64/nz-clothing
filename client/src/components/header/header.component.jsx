f153e9a367bb2cd496585c04dfa09a76f50ec3ea
torrent benoit <torrentdu64@gmail.com>
2019-12-28 10:49:41 +1300

    separate 2 folder client react app and express server for backend payment

---
+++ client/src/components/header/header.component.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { createStructuredSelector } from 'reselect';
+
+
+
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+
+// import './header.styles.scss';
+import { HeaderContainer, LogoContainer, OptionContainer , OptionDiv, OptionLink} from './hearder.styles';
+
+
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdow.component';
+
+import  { selectCardHidden} from '../../redux/cart/cart.selectors';
+import  {selectCurrentUser} from '../../redux/user/user.selectors';
+import { signOutStart } from '../../redux/user/user.actions';
+
+const Header = ({ currentUser , hidden, signOutStart}) => (
+  <HeaderContainer>
+    <LogoContainer to='/'>
+      <Logo className='logo' />
+    </LogoContainer>
+    <OptionContainer>
+      <OptionLink to='/shop'>
+        SHOP
+      </OptionLink>
+      <OptionLink to='/shop'>
+        CONTACT
+      </OptionLink>
+      {currentUser ? (
+        <OptionDiv onClick={signOutStart}>
+          SIGN OUT
+        </OptionDiv>
+      ) : (
+        <OptionLink to='/signin'>
+          SIGN IN
+        </OptionLink>
+      )}
+      <CartIcon />
+    </OptionContainer>
+    { hidden ? null : <CartDropdown />}
+    
+  </HeaderContainer>
+);
+
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCardHidden
+});
+
+const mapDispatchToProps = dispatch =>({
+  signOutStart: () => dispatch(signOutStart())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      methode: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then( (res) => {
+      alert('success')
+    }).catch(err => {
+      alert('error')
+    })
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
