40017f98a49d6c62b44566c25f82d1ef9ddf442c
torrent benoit <torrentdu64@gmail.com>
2020-01-02 12:56:37 +1300

    add lazy and suspanse to router and error boundary catch error occur

---
+++ client/src/components/error-boundary/error-boundary.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { ErrorImageContainer, ErrorImageOverlay, ErrorImageText} from './error-boundary.styles';
+
+class ErrorBoundary extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            hasErrored: false
+        }
+    }
+    static getDerivedStateFromError(error) {
+        return { hasErrored: true };
+    }
+
+    componentDidCatch(error, info) {
+        console.log(error);
+    }
+    
+    render(){
+        if(this.state.hasErrored){
+            return (<ErrorImageOverlay>
+                    <ErrorImageContainer imageUrl='https://i.imgur.com/O0DCcQy.png'/>
+                    <ErrorImageText>Sorry ...</ErrorImageText>
+                </ErrorImageOverlay>)
+        }
+        return this.props.children;
+    }
+
+}
+
+export default ErrorBoundary;
--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -4,15 +4,15 @@ import 'firebase/auth';
 
 
 const config =  {
-    apiKey: "AIzaSyDM9o5GTokE9FbTqQ9aLSj0WuQ5aTVYPb0",
-    authDomain: "nz-db-9c284.firebaseapp.com",
-    databaseURL: "https://nz-db-9c284.firebaseio.com",
-    projectId: "nz-db-9c284",
-    storageBucket: "nz-db-9c284.appspot.com",
-    messagingSenderId: "648810017281",
-    appId: "1:648810017281:web:3f7fa420629a8b0c104600",
-    measurementId: "G-78F8EF886H"
-  };
+  apiKey: "AIzaSyDM9o5GTokE9FbTqQ9aLSj0WuQ5aTVYPb0",
+  authDomain: "nz-db-9c284.firebaseapp.com",
+  databaseURL: "https://nz-db-9c284.firebaseio.com",
+  projectId: "nz-db-9c284",
+  storageBucket: "nz-db-9c284.appspot.com",
+  messagingSenderId: "648810017281",
+  appId: "1:648810017281:web:3f7fa420629a8b0c104600",
+  measurementId: "G-78F8EF886H"
+};
 
   firebase.initializeApp(config);
 
