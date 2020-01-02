dc3f6e026c46e24ad482e1ceae90aaffa8450e83
torrent benoit <torrentdu64@gmail.com>
2020-01-03 12:43:09 +1300

    change in server.js compression in production

--- server.js
+++ server.js
@@ -13,25 +13,26 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(compression());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(cors());
-
+ 
 if (process.env.NODE_ENV === 'production') {
-    app.use(enforce.HTTPS({ trustProtoHeader: true }));
-    app.use(express.static(path.join(__dirname, 'client/build')));
-   
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
- }
-
+  app.use(compression);
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  app.use(express.static(path.join(__dirname, 'client/build')));
+ 
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+ 
 app.listen(port, error => {
-    if(error) throw error;
-    console.log('server running on port'+ port);
+  if (error) throw error;
+  console.log('Server is running on port ' + port);
 });
+
 
 app.get('/service-worker.js', (req, res) =>{
     res.sendFile(path.resolve(__dirname, '..', 'build', 'servise-worker.js'));
