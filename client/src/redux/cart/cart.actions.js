f6e1cf7c6d2a9827f8ff5b2a8e966418c2254442
torrent benoit <torrentdu64@gmail.com>
2020-01-04 11:29:18 +1300

    update saga for handle the authrization

--- client/src/redux/cart/cart.actions.js
+++ client/src/redux/cart/cart.actions.js
@@ -18,9 +18,17 @@ export const removeItem =  item => ({
 export const clearItemFromCart = item => ({
     type: CartActionTypes.CLEAR_ITEM_FROM_CART,
     payload: item
-})
+});
 
 export const clearCart = () => ({
   type: CartActionTypes.CLEAR_CART
+});
 
-})
+export const updateCartInFirebase = () => ({
+  type: CartActionTypes.UPDATE_CART_IN_FIREBASE
+});
+
+export const setCartFromFirebase = cartItems => ({
+  type: CartActionTypes.SET_CART_FROM_FIREBASE,
+  payload: cartItems
+});
